e961cd64bca5c39ccf86074788abcbe58a9f6dc3
pfnazhmi <pfnazhmi@gmail.com>
2024-03-24 12:27:50 +0700

    add: onboarding page

---
+++ lib/pages/onboarding/onboarding_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OnboardPage extends StatelessWidget {
+  const OnboardPage({
+    Key? key,
+    this.text,
+    this.image,
+    this.title,
+  }) : super(key: key);
+
+  final String? text, image, title;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 12,
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Image.asset(
+              image!,
+              height: 350,
+              width: 343,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              title!, // Menggunakan Text untuk menampilkan judul
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                fontSize: 24,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Text(
+              text!,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                fontSize: 14,
+                color: const Color(0xFF7A7A7A),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/theme/app_assets.dart
+++ lib/theme/app_assets.dart
@@ -1,1 +1,5 @@
-
+class AppAsset {
+  static const onboard1 = 'assets/images/onboard1.png';
+  static const onboard2 = 'assets/images/onboard2.png';
+  static const onboard3 = 'assets/images/onboard3.png';
+}
