2b692a8771a2d681e557f3f991795ba685d49348
pfnazhmi <pfnazhmi@gmail.com>
2024-03-24 12:47:38 +0700

    add feat: onboarding page

--- lib/theme/app_assets.dart
+++ lib/theme/app_assets.dart
@@ -1,1 +1,5 @@
-
+class AppAssets {
+  static const onboarding1 = 'assets/images/onboard1.png';
+  static const onboarding2 = 'assets/images/onboard2.png';
+  static const onboarding3 = 'assets/images/onboard3.png';
+}
